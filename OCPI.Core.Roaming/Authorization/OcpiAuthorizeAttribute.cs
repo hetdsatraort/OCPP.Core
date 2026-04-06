39da2f2d39d4d676a6822f32879a210762523ee5
Het Satra <het.satra@onerooftech.com>
2026-04-06 19:03:32 +0530

    ocpi auth attribute added

---
+++ OCPI.Core.Roaming/Authorization/OcpiAuthorizeAttribute.cs
@@ -0,0 +1,27 @@
+using BitzArt;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+public class OcpiAuthorizeAttribute : Attribute, IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        //var token = GetToken(context.HttpContext.Request);
+
+        // Your authorization logic.
+    }
+
+    private static string GetToken(HttpRequest request)
+    {
+        var authHeaders = request.Headers["Authorization"];
+
+        if (!authHeaders.Any()) throw ApiException.Unauthorized("Authorization header not found");
+        if (authHeaders.Count > 1) throw ApiException.Unauthorized("Multiple Authorization headers not allowed.");
+
+        var header = authHeaders.First()!;
+        if (string.IsNullOrWhiteSpace(header)) throw ApiException.Unauthorized("Invalid authorization token.");
+
+        var token = header.Split(" ").Last();
+
+        return token;
+    }
+}
