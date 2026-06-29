f110cc4c6745ec59151e35f1e16404489f8dcc0f
Het Satra <het.satra@onerooftech.com>
2026-06-29 11:21:49 +0530

    vehicle id added in session and migrated

---
+++ OCPP.Core.Database/Migrations/20260629054959_VehicleIdAdded.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OCPP.Core.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class VehicleIdAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "VehicleId",
+                table: "ChargingSessions",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChargingSessions_VehicleId",
+                table: "ChargingSessions",
+                column: "VehicleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChargingSession_UserVehicle",
+                table: "ChargingSessions",
+                column: "VehicleId",
+                principalTable: "UserVehicles",
+                principalColumn: "RecId",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChargingSession_UserVehicle",
+                table: "ChargingSessions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ChargingSessions_VehicleId",
+                table: "ChargingSessions");
+
+            migrationBuilder.DropColumn(
+                name: "VehicleId",
+                table: "ChargingSessions");
+        }
+    }
+}
